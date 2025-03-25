d654aace0dcadf76b1ac1dc3783ef225110c5755
Xingzhe (Eric) Li <lamblamb@users.noreply.huggingface.co>
2025-03-25 11:53:48 -0700

    more fixes, experiment cost function

--- scaling.py
+++ scaling.py
@@ -87,29 +87,9 @@ def scaling_law_function(settings, params, constants):
         + params["B"] / (data_size ** params["beta"])
     )
 
-
-# for our functional hypothesis
-class ScalingLaw:
-    def __init__(self):
-        self.params = {
-            "A": None,
-            "alpha": None,
-            "B": None,
-            "beta": None,
-            "optimal_loss": None,
-        }
-        for p in self.params:
-            self.params[p] = np.linspace(1e-6, 10, 1000)
-
-    def __call__(self, model_size, data_size):
-        return scaling_law_function((model_size, data_size), self.params, ())
-
-
-my_scale_law = ScalingLaw()
-
-
 # run an experiment and get the loss
 def run_experiment(experiment_settings):
+    # TODO: need a way to track number of epochs already ran when loading a checkpoint
     model = init_model(experiment_settings)
     data = init_data(experiment_settings)
     loss = None
@@ -126,11 +106,24 @@ def run_experiment(experiment_settings):
     return loss
 
 
-class ScalingLawsBayesianOptimization:
-    def __init__(self, budget, param_space_size=50000):
+class ScalingLawsBayesianOptimization(OptBayesExptNoiseParameter):
+    def __init__(self, budget, possible_experiment_settings, param_space_size=50000):
         self.param_space_size = param_space_size
         self.budget = budget
         self.params = self.initial_scaling_law_params(param_space_size)
+
+        data_size = np.linspace(
+            possible_experiment_settings["data_size"]["min_value"],
+            possible_experiment_settings["data_size"]["max_value"],
+        )
+        model_size = np.linspace(
+            possible_experiment_settings["model_size"]["min_value"],
+            possible_experiment_settings["model_size"]["max_value"],
+        )
+
+        settings = (data_size, model_size)
+        constants = ()
+        OptBayesExptNoiseParameter.__init__(self, scaling_law_function, settings, self.params, constants)
 
     def initial_scaling_law_params(self):
         # initial ranges based on Chinchilla scaling laws
@@ -146,46 +139,35 @@ class ScalingLawsBayesianOptimization:
         return A, alpha, B, beta, optimal_loss, sigs
 
     def scaling_law_function(self, settings, params, constants):
+        # TODO: make settings the layer numbers, hidden dimensions, etc. instead of parameter count
+        # TODO: or calculate the layer numbers, hidden dimensions, etc. from the parameter count
         model_size, data_size = settings
         A, alpha, B, beta, optimal_loss, sigs = params
         return optimal_loss + A / (model_size**alpha) + B / (data_size**beta)
+    
+    def cost_estimate(self):
+        """Returns cost of experiments, used in utility function"""
+        return cost_function(self.allsettings[0], self.allsettings[1], self.budget)
 
-    def iterate(self, possible_experiment_settings):
+    def iterate(self):
         # choose experiment settings with optbayes
         # run an experiment
         # refine the scaling law with the loss and optbayes
         # repeat until you use up the budget
-        data_size = np.linspace(
-            possible_experiment_settings["data_size"]["min_value"],
-            possible_experiment_settings["data_size"]["max_value"],
-        )
-        model_size = np.linspace(
-            possible_experiment_settings["model_size"]["min_value"],
-            possible_experiment_settings["model_size"]["max_value"],
-        )
+        
+        chosen_settings = self.opt_setting()
 
-        settings = (data_size, model_size)
-        constants = ()
+        loss = run_experiment(chosen_settings)
 
-        my_obe = OptBayesExptNoiseParameter(
-            scaling_law_function,
-            settings,
-            self.params,
-            constants,
-            noise_parameter_index=len(self.params) - 1,
-            scale=False,
-        )
+        compute += flops(chosen_settings[0], chosen_settings[1])
 
+        measure = (chosen_settings, loss)
+        self.pdf_update(measure)
+        
+    def run(self):
         compute = 0
         while compute < self.budget:
-            chosen_settings = my_obe.opt_setting()
-
-            loss = run_experiment(chosen_settings)
-
-            compute += flops(chosen_settings[0], chosen_settings[1])
-
-            measure = (chosen_settings, loss)
-            my_obe.update_pdf(measure)
+            self.iterate()
 
 
 def main():
